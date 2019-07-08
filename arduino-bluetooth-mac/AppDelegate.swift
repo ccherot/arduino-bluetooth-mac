a7f3f56dd4bcdc925aa05e7761eb69e35ef3b6da
Colin Cherot <ccherot@sonic.net>
2019-07-08 16:29:04 -0700

    Initial Commit

---
+++ arduino-bluetooth-mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  arduino-bluetooth-mac
+//
+//  Created by Colin Cherot on 7/8/19.
+//  Copyright © 2019 Colin Cherot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ arduino-bluetooth-macTests/arduino_bluetooth_macTests.swift
@@ -0,0 +1,34 @@
+//
+//  arduino_bluetooth_macTests.swift
+//  arduino-bluetooth-macTests
+//
+//  Created by Colin Cherot on 7/8/19.
+//  Copyright © 2019 Colin Cherot. All rights reserved.
+//
+
+import XCTest
+@testable import arduino_bluetooth_mac
+
+class arduino_bluetooth_macTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
